fd0b133aab33b11ef6fab77bcf65376c41e8b02c
Safa Yousif <safa.yousif@outlook.com>
2022-07-24 20:56:31 +0800

    initial commit

---
+++ assets/data/characters.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: "demo name",
+        age: "demo age",
+        hobbies: "demo hobby",
+        demo: "desc",
+        demo: "img url",
+    },
+    {
+        id: 1,
+        name: "demo name",
+        age: "demo age",
+        hobbies: "demo hobby",
+        demo: "desc",
+        demo: "img url",
+    },
+    {
+        id: 1,
+        name: "demo name",
+        age: "demo age",
+        hobbies: "demo hobby",
+        demo: "desc",
+        demo: "img url",
+    }
+]
---
+++ assets/data/works.js
@@ -0,0 +1,7 @@
+export default [
+    "demo url 1",
+    "demo url 2",
+    "demo url 3",
+    "demo url 4",
+    "demo url 5",
+]
