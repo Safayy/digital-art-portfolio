69ecb952277c1b436452eb92db9e814c46f212e6
Safa Yousif <safa.yousif@outlook.com>
2022-08-02 11:54:03 +0800

    static coded char catalog

--- assets/data/art.js
+++ assets/data/art.js
@@ -7,15 +7,46 @@ export default {
     "characters":[
         {
             //REQUIRED ATTRIBTUES
-            name: "Rebecca Sea",
+            name: "Rebecca White",
             isAdopted: false,
             faceLink: "char1-face.png",
             bodyLink: "char1-body.png",
-            description: "",
+            story: "Est consectetur labore dolor cupidatat ex est qui laborum laborum in duis aliquip nostrud. Eu nulla ad excepteur dolor nostrud amet elit sunt incididunt cupidatat excepteur",
             //OPTIONAL ATTRIBUTES
             customTags: {
-                dob: "",
-                hobbies:"",
+                "dob": "",
+                "origin": "",
+                "hobbies":"",
+                "kill count": 2342
+            }
+        },{
+            //REQUIRED ATTRIBTUES
+            name: "2 White",
+            isAdopted: false,
+            faceLink: "char1-face.png",
+            bodyLink: "char1-body.png",
+            story: "Est consectetur labore dolor cupidatat ex est qui laborum laborum in duis aliquip nostrud. Eu nulla ad excepteur dolor nostrud amet elit sunt incididunt cupidatat excepteur",
+            //OPTIONAL ATTRIBUTES
+            customTags: {
+                "dob": "",
+                "origin": "",
+                "hobbies":"",
+                "kill count": 2342
+            }
+        },
+        {
+            //REQUIRED ATTRIBTUES
+            name: "3 White",
+            isAdopted: false,
+            faceLink: "char1-face.png",
+            bodyLink: "char1-body.png",
+            story: "Est consectetur labore dolor cupidatat ex est qui laborum laborum in duis aliquip nostrud. Eu nulla ad excepteur dolor nostrud amet elit sunt incididunt cupidatat excepteur",
+            //OPTIONAL ATTRIBUTES
+            customTags: {
+                "dob": "",
+                "origin": "",
+                "hobbies":"",
+                "kill count": 2342
             }
         }
     ],
